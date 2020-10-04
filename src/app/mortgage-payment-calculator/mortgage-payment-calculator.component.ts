729e667489e67f46cc7ef07cc192a036b6be3196
Prabhakaran MANICKKAVEL <prabhakaran@imagia.com>
2020-10-04 12:45:08 -0400

    Create template, Styling and createMortageForm with FormBuilder

--- src/app/mortgage-payment-calculator/mortgage-payment-calculator.component.ts
+++ src/app/mortgage-payment-calculator/mortgage-payment-calculator.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-mortgage-payment-calculator',
   templateUrl: './mortgage-payment-calculator.component.html',
-  styleUrls: ['./mortgage-payment-calculator.component.scss']
+  styleUrls: ['./mortgage-payment-calculator.component.scss'],
 })
 export class MortgagePaymentCalculatorComponent implements OnInit {
+  public formGroup: FormGroup;
+  public titleAlert = 'This field is required';
+  public paymentFrequencyOptions = [];
+  public terms = [];
+  public prePaymentFrequencyOptions = [];
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.createMortgageForm();
   }
 
+  // Creating a mortgage form
+  public createMortgageForm(): void {
+    this.formGroup = this.formBuilder.group({});
+  }
+  // Calculate result and open the mortgage summary form
+  public onCalculate(): void {}
 }
--- src/app/mortgage-summary/mortgage-summary.component.ts
+++ src/app/mortgage-summary/mortgage-summary.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface MortgageSummaryData {
+  mortgageAmount: number;
+  amortizationPeriod: number;
+  paymentFrequency: number;
+  interestRate: number;
+  totalInterestCost?: number;
+  term: number;
+}
+
 @Component({
   selector: 'app-mortgage-summary',
   templateUrl: './mortgage-summary.component.html',
-  styleUrls: ['./mortgage-summary.component.scss']
+  styleUrls: ['./mortgage-summary.component.scss'],
 })
 export class MortgageSummaryComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  //Calculating the Payment Amount per Period
+  public mortgagePayment(): number {
+    return;
   }
-
+  // Calculate Total Interest Cost by amortization periods
+  public totalInterestCost(): number {
+    return;
+  }
+  // Calculate Total Cost including interest
+  public totalCost(): number {
+    return;
+  }
 }
