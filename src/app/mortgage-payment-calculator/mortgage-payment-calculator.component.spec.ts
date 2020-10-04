33fecef53bc3e500f162aa2a0a2a7ffcc7105273
Prabhakaran MANICKKAVEL <prabhakaran@imagia.com>
2020-10-04 14:17:32 -0400

    Add test

--- src/app/mortgage-payment-calculator/mortgage-payment-calculator.component.spec.ts
+++ src/app/mortgage-payment-calculator/mortgage-payment-calculator.component.spec.ts
@@ -1,17 +1,25 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MortgagePaymentCalculatorComponent } from './mortgage-payment-calculator.component';
+import { FormsModule, ReactiveFormsModule, FormGroup } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '../../material-module';
 
 describe('MortgagePaymentCalculatorComponent', () => {
   let component: MortgagePaymentCalculatorComponent;
   let fixture: ComponentFixture<MortgagePaymentCalculatorComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ MortgagePaymentCalculatorComponent ]
-    })
-    .compileComponents();
-  });
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [MortgagePaymentCalculatorComponent],
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        MaterialModule,
+        BrowserAnimationsModule,
+      ],
+    }).compileComponents();
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(MortgagePaymentCalculatorComponent);
@@ -21,5 +29,28 @@ describe('MortgagePaymentCalculatorComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should create a FormGroup', () => {
+    component.ngOnInit();
+    expect(component.formGroup instanceof FormGroup).toBe(true);
+  });
+
+  it('should create a form with the summary data', () => {
+    expect(component.formGroup.contains('mortgageAmount')).toBeTruthy();
+    expect(component.formGroup.contains('amortizationPeriod')).toBeTruthy();
+    expect(component.formGroup.contains('paymentFrequency')).toBeTruthy();
+    expect(component.formGroup.contains('interestRate')).toBeTruthy();
+  });
+
+  it('should consider mortgageAmount and interest rate to required', () => {
+    let mortgageAmountRequired = component.formGroup.get('mortgageAmount');
+    let interestRateRequired = component.formGroup.get('interestRate');
+
+    mortgageAmountRequired.setValue('');
+    interestRateRequired.setValue('');
+
+    expect(mortgageAmountRequired.valid).toBeFalsy();
+    expect(interestRateRequired.valid).toBeFalsy();
   });
 });
--- src/app/mortgage-summary/mortgage-summary.component.spec.ts
+++ src/app/mortgage-summary/mortgage-summary.component.spec.ts
@@ -1,25 +1,32 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MortgageSummaryComponent } from './mortgage-summary.component';
+import {
+  MAT_DIALOG_DATA,
+  MatDialogModule,
+  MatDialogRef,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('MortgageSummaryComponent', () => {
   let component: MortgageSummaryComponent;
   let fixture: ComponentFixture<MortgageSummaryComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ MortgageSummaryComponent ]
-    })
-    .compileComponents();
-  });
+  const dialogMock = {
+    close: () => {},
+  };
 
-  beforeEach(() => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [MortgageSummaryComponent],
+      imports: [MatDialogModule, BrowserAnimationsModule],
+      providers: [
+        { provide: MatDialogTitle, useValue: {} },
+        { provide: MatDialogRef, useValue: dialogMock },
+        { provide: MAT_DIALOG_DATA, useValue: [] },
+      ],
+    });
     fixture = TestBed.createComponent(MortgageSummaryComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  }));
 });
